ba1fc05ac35f478e1afd39c0fee63cf0eb6dea98
Argatrus <raja.aarav@gmail.com>
2024-05-27 13:38:45 +0530

    Changed comment

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import customtkinter as ctk
 
 ctk.set_default_color_theme("dark-blue")
 
-class App(ctk.CTk): # somethign
+class App(ctk.CTk): # Main Menu
     def __init__(self):
         super().__init__()
         self.wm_title('Main Menu')
