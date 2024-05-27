c8da2198e222b5566e00a936752ec309cfc4290a
Argatrus <raja.aarav@gmail.com>
2024-05-27 15:57:11 +0530

    reduced reliance on strings and more towards code words or None

--- classes.py
+++ classes.py
@@ -3,7 +3,7 @@ multipliers = [0.75, 0.8, 0.85, 0.95, 1, 1.05, 1.1, 1.15]
 
 class Parent: # main parent class for all characters
     def __init__(self, name):
-        self.name = name # can name characters and rename probably
+        self.name = name # can name characters (maybe rename)...
         self.exp = [0, 100]
         self.level = 1
         self.health = 100
@@ -16,7 +16,6 @@ class Parent: # main parent class for all characters
     def level_up(self):
         self.level += 1
         self.exp = [0, self.exp[1]+20]
-        return f'{self.name} leveled up!'
 
     # Going to use a randomiser to handle all combat stuff making it like a classic crpg.
 
@@ -29,12 +28,12 @@ class Parent: # main parent class for all characters
             self.attacks[1] -= 1
             return self.attacks[name][0] * random.choice(multipliers)
         else:
-            return 'does nothing'
+            return 'nopp'
 
     def heal(self):
         if self.heals[0] > 0:
             self.health += self.heals[1] * random.choice(multipliers)
             self.heals[0] -= 1
-            return 'Heals'
+            return None
         else:
-            return 'does nothing'
+            return 'nopp'
